a46e7b015810561f2fd9477157f0c65dacc02c67
av8ta <av8ta@pm.me>
2022-12-14 13:13:18 +0800

    add data types and change marker to circle

--- ui/index.ts
+++ ui/index.ts
@@ -1,14 +1,15 @@
 import './index.css'
 import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
+import L, { LatLngTuple } from 'leaflet'
 import websocket from 'callbag-websocket'
 import observe from 'callbag-observe'
+import { AdsbData } from './types'
 
 const HOST = process.env.websocket_host || 'localhost'
 const PORT = Number.parseInt(process.env.websocket_port || '2222', 10)
 const adsb$ = websocket(`ws://${HOST}:${PORT}`)
 
-const location = [-8.580189, 115.278943]
+const location: LatLngTuple = [-8.580189, 115.278943]
 
 const map = L.map('map').setView(location, 10)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,18 +17,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
-const marker = L.marker(location).addTo(map)
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
+L.marker(location).addTo(map)
 
-observe((message: { data: any }) => {
-  const adsbData = JSON.parse(message.data)
+observe((message: { data: string }) => {
+  const adsbData: AdsbData = JSON.parse(message.data)
   console.log(adsbData)
   if (adsbData.parsed) {
     const { callsign, lon, lat, altitude, speed, heading, timestamp } = adsbData.parsed
-    // const position = [Number.parseFloat(lat), Number.parseFloat(lon)]
-    const position = [lat, lon]
-    // console.log(position, lon, lat)
-    const marker = L.marker(position).addTo(map)
-    marker.bindPopup(`<b>${callsign ? `${callsign} at ` : ''} ${altitude} feet bearing ${heading}° at ${speed} knots</b></br><p>${Date(timestamp)}</p>`)
+    const position: LatLngTuple = [lat, lon]
+    const circle = L.circle(position, {
+      color: 'blue',
+      fillColor: 'aqua',
+      fillOpacity: 0.5,
+      radius: 400
+    }).addTo(map)
+    circle.bindPopup(`<b>${callsign ? `${callsign} at ` : ''} ${altitude} feet bearing ${heading}° at ${speed} knots</b></br><p>${Date(timestamp)}</p>`)
   }
 })(adsb$)
---
+++ ui/types.ts
@@ -0,0 +1,15 @@
+export interface AdsbData {
+  message: string,
+  decoded: string,
+  parsed: Position
+}
+
+export interface Position {
+  callsign: string,
+  lon: number,
+  lat: number,
+  altitude: number,
+  speed: number,
+  heading: number,
+  timestamp: number
+}
